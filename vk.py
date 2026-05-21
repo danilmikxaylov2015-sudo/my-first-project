78607c7321a16c22671a9be8252c0253e070a82d
danilmikxaylov2015-sudo <danilmikxaylov2015@gmail.com>
2026-05-21 20:02:32 +0300

    Update vk.py

--- vk.py
+++ vk.py
@@ -4,7 +4,6 @@ import time
 import random
 import threading
 
-# Автоустановка vk_api
 try:
     import vk_api
     from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
@@ -16,11 +15,10 @@ except ImportError:
     from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 
-# ВСТАВЬ ТОЛЬКО ТОКЕН СООБЩЕСТВА
 TOKEN = "vk1.a.jmhGtKNRy-okO7WM6HyGJofKiJMaUnBDyB3kEqxdKypWpcnJaEB7KBJixSmIMLc7YLBJHu6wKY2sElm6VlK59GWdnir2DJQl5D9ohPLQ_8USyg-_gpviWLw31YaUIcx51Y84dSXBPjUpwIULup3JGkiHECtNOGSqlxX4q3IvWgeGEwzaXefqwmTa9aFx2-g9b5dmx07Wx-HH3-Tu_2HDag"
 
 PR_TEXT = "пишите в лс пж. [https://vk.com/danil_mikxaylov|Данил Михайлов]"
-DELAY = 5
+DELAY = 3
 
 vk_session = vk_api.VkApi(token=TOKEN)
 vk = vk_session.get_api()
@@ -31,24 +29,10 @@ active_chats = {}
 def get_group_id():
     try:
         info = vk.groups.getById()
-        group_id = info[0]["id"]
-        print("ID сообщества найден:", group_id)
-        return group_id
+        return info[0]["id"]
     except Exception as e:
-        print("Не получилось получить ID через groups.getById:", e)
-
-    try:
-        info = vk.groups.getTokenPermissions()
-        if "group_id" in info:
-            group_id = info["group_id"]
-            print("ID сообщества найден:", group_id)
-            return group_id
-    except Exception as e:
-        print("Не получилось получить ID через groups.getTokenPermissions:", e)
-
-    print("ВК не дал узнать ID сообщества по токену.")
-    print("Проверь, что это именно токен сообщества, а не пользователя.")
-    sys.exit()
+        print("Ошибка получения ID сообщества:", e)
+        sys.exit()
 
 
 GROUP_ID = get_group_id()
@@ -67,7 +51,7 @@ def pr_loop(peer_id):
     while active_chats.get(peer_id, False):
         try:
             send_message(peer_id, PR_TEXT)
-            print("Пиар отправлен")
+            print(f"Пиар отправлен в чат {peer_id}")
         except Exception as e:
             print("Ошибка отправки:", e)
 
@@ -75,8 +59,7 @@ def pr_loop(peer_id):
 
 
 print("VK пиар-бот запущен.")
-print("Напиши /start в беседе, чтобы запустить пиар.")
-print("Напиши /stop, чтобы остановить.")
+print("Команды: /start и /stop")
 
 
 for event in longpoll.listen():
